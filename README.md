80f94c0b289f2026c7625203c15bf661c3301b55
Bobby Galli <bobbyg603@gmail.com>
2022-11-25 22:02:19 -0500

    chore: initial release (#1)

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# ZipCreator
-🐳➡️🐟 .NET Standard 2.0 zip file creator with glob support
+[![BugSplat Banner Image](https://user-images.githubusercontent.com/20464226/149019306-3186103c-5315-4dad-a499-4fd1df408475.png)](https://bugsplat.com)
+
+# BugSplat
+### **Crash and error reporting built for busy developers.**
+
+[![Follow @bugsplatco on Twitter](https://img.shields.io/twitter/follow/bugsplatco?label=Follow%20BugSplat&style=social)](https://twitter.com/bugsplatco)
+[![Join BugSplat on Discord](https://img.shields.io/discord/664965194799251487?label=Join%20Discord&logo=Discord&style=social)](https://discord.gg/bugsplat)
+
+## 👋 Introduction
+
+ZipCreator is .NET Standard 2.0 library for zip file creation with glob support.
+
+## ⚙️ Installation
+
+ZipCreator can be installed via NuGet.
+
+```sh
+Install-Package ZipCreator
+```
+
+## 🧑‍💻 Usage
+
+Add a using statement for the `Zc` namespace
+
+```cs
+using Zc;
+```
+
+You can create an instance of `ZipCreator` from a text file that contains a list of filters separated by new lines.
+
+```cs
+// manifest.txt
+// path/to/folder/**/*
+// README.md
+// LICENSE.md
+var zipCreator = ZipCreator.CreatFromFile("manifest.txt");
+```
+
+You can also creat an instances of `ZipCreator` with the default constructor and set the `Settings` properties according to your use case.
+
+```cs
+var zipCreator = new ZipCreator();
+zipCreator.Settings.Filters = new List<string>() { "path/to/folder/**/*" };
+zipCreator.Settings.Interceptor = (fileInfo) => Debug.WriteLine(fileInfo.FullName);
+zipCreator.Settings.Overwrite = true;
+zipCreator.Settings.ZipOutputFile = new FileInfo(pathToOutputZip);
+```
+
+The `Filters` property accepts a list of path globs. You can also exclude patterns by prefixing the pattern with a `!`.
+
+```cs
+zipCreator.Settings.Filters = new List<string>()
+{
+    "path/to/folder/**/*",
+    "!path/to/folder/exclude.txt"
+};
+```
+
+The `Interceptor` property is an `Action<FileInfo>` that get's invoked with each file that will be added to the zip and is useful if you'd like to run an action on certain files before adding them to the zip.
+
+```cs
+zipCreator.Settings.Interceptor = (fileInfo) => SignTool(fileInfo.FullName);
+```
+
+The `Overwrite` property controls whether the `ZipOutputFile` file should be overwritten if it exists. If `Overwrite` is false the call to `MakeZips` will throw if `ZipOutputFile` exists.
+
+
+## 🐛 About
+
+[BugSplat](https://bugsplat.com) is a software crash and error reporting service with support for [Windows C++](https://docs.bugsplat.com/introduction/getting-started/integrations/desktop/cplusplus), [.NET Framework](https://docs.bugsplat.com/introduction/getting-started/integrations/desktop/windows-dot-net-framework), [dotnet](https://docs.bugsplat.com/introduction/getting-started/integrations/cross-platform/dot-net-standard) and [many more](https://docs.bugsplat.com/introduction/getting-started/integrations). BugSplat automatically captures critical diagnostic data such as stack traces, log files, and other runtime information. BugSplat also provides automated incident notifications, a convenient dashboard for monitoring trends and prioritizing engineering efforts, and integrations with popular development tools to maximize productivity and ship more profitable software.
---
+++ ZipCreatorTest/ZipCreatorTest.cs
@@ -0,0 +1,154 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using Zc;
+
+namespace ZipCreatorTest
+{
+    public class Tests
+    {
+        private string inputFile;
+        private string inputFileContents;
+        private string testFolder;
+        private List<string> testFiles;
+        private string outputZip;
+
+        [SetUp]
+        public void Setup()
+        {
+            testFolder = "test";
+            testFiles = new List<string>()
+            {
+                "test.txt",
+                "test.exe",
+                "test.dll"
+            };
+
+            Directory.CreateDirectory(testFolder);
+
+            foreach (var testFile in testFiles)
+            {
+                File.WriteAllText(Path.Combine(testFolder, testFile), testFile);
+            }
+
+            inputFile = "input.txt";
+            inputFileContents = $"{testFolder}/*";
+            outputZip = "output.zip";
+
+            File.WriteAllText(inputFile, inputFileContents);
+        }
+
+        [Test]
+        public void CreateFromFile_WithInvalidFile_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => ZipCreator.CreateFromFile(new FileInfo("invalid")));
+        }
+
+        [Test]
+        public void CreateFromFile_WithValidFile_SetsValueOfFilters()
+        {
+            var zipCreator = ZipCreator.CreateFromFile(new FileInfo(inputFile));
+
+            Assert.That(zipCreator.Settings.Filters[0], Is.EqualTo(inputFileContents));
+        }
+
+        [Test]
+        public void MakeZips_WithGlobInput_CreatesZipWithFilesMatchingGlob()
+        {
+            var extension = ".exe";
+            var zipCreator = new ZipCreator();
+            zipCreator.Settings.Filters = new List<string>() { $"{testFolder}/*{extension}" };
+            zipCreator.Settings.ZipOutputFile = new FileInfo(outputZip);
+
+            var result = zipCreator.MakeZips();
+
+            using (var archive = ZipFile.OpenRead(result.FullName))
+            {
+                var expected = testFiles.Where(file => file.EndsWith(extension)).ToList();
+                var results = archive.Entries.Select(entry => entry.Name).ToList();
+                CollectionAssert.AreEqual(expected, results);
+            }
+        }
+
+        [Test]
+        public void MakeZips_WithExcludeFilter_CreatesZipIgnoringExcludeFilters()
+        {
+            var extension = ".txt";
+            var zipCreator = new ZipCreator();
+            zipCreator.Settings.Filters = new List<string>() {
+                $"{testFolder}/*",
+                $"!{testFolder}/*{extension}"
+            };
+            zipCreator.Settings.ZipOutputFile = new FileInfo(outputZip);
+
+            var result = zipCreator.MakeZips();
+
+            using (var archive = ZipFile.OpenRead(result.FullName))
+            {
+                var expected = testFiles.Where(file => !file.EndsWith(extension)).OrderBy(name => name);
+                var results = archive.Entries.Select(entry => entry.Name).OrderBy(name => name);
+                CollectionAssert.AreEqual(expected, results);
+            }
+        }
+
+        [Test]
+        public void MakeZips_WithInterceptor_CallsInterceptorWithEachFile()
+        {
+            var results = new List<string>();
+            var zipCreator = new ZipCreator();
+            zipCreator.Settings.Filters = new List<string>() { $"{testFolder}/*" };
+            zipCreator.Settings.Interceptor = (fileInfo) => results.Add(fileInfo.Name);
+            zipCreator.Settings.ZipOutputFile = new FileInfo(outputZip);
+
+            zipCreator.MakeZips();
+
+            var expected = testFiles.OrderBy(name => name);
+            var actual = results.OrderBy(name => name);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void MakeZips_WithOverwriteFalse_ThrowsTryingToOverwriteFile()
+        {
+            var zipCreator = ZipCreator.CreateFromFile(new FileInfo(inputFile));
+            zipCreator.Settings.Overwrite = false;
+            zipCreator.Settings.ZipOutputFile = new FileInfo(outputZip);
+            File.WriteAllText(outputZip, "hello world!");
+
+            Assert.Throws<Exception>(() => zipCreator.MakeZips());
+        }
+
+        [Test]
+        public void MakeZips_WithOverwriteTrue_OverwritesOutputFile()
+        {
+            var zipCreator = ZipCreator.CreateFromFile(new FileInfo(inputFile));
+            zipCreator.Settings.Overwrite = true;
+            zipCreator.Settings.ZipOutputFile = new FileInfo(outputZip);
+            File.WriteAllText(outputZip, "hello world!");
+
+            Assert.DoesNotThrow(() => zipCreator.MakeZips());
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (File.Exists(inputFile))
+            {
+                File.Delete(inputFile);
+            }
+
+            if (File.Exists(outputZip))
+            {
+                File.Delete(outputZip);
+            }
+
+            if (Directory.Exists(testFolder))
+            {
+                Directory.Delete(testFolder, true);
+            }
+        }
+    }
+}
